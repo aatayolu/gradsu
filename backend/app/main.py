7ad52e2178ea72aa58dea2174785b33983888e06
aatayolu <ahmetatayolu@gmail.com>
2024-05-10 16:31:06 +0300

    mongo-deneme-v4

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,8 @@ def read_root():
 def check_connection():
     try:
         mongodb_url = os.getenv("MONGODB_URL")
-        client = MongoClient(mongodb_url)
+        client =  MongoClient(mongodb_url)
+        # client = MongoClient('mongodb+srv://zeynepkrtls01:ZRAZ2x5rw9AXMllc@sugradcluster.aro7tnh.mongodb.net/?ssl=true')
         # Try to list databases, which will check the connection
         client.list_database_names()
         return {"status": "MongoDB connection is successful"}

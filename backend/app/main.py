4ab7c98d17588376e83fb33375e884644e44a7cb
aatayolu <ahmetatayolu@gmail.com>
2024-05-22 16:57:50 +0300

    CORS-v2

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,7 +51,7 @@ origins = ['https://localhost:3000']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

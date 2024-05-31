ec8ddd840e44a394ae2c6b77d85cec6c9ab85ebf
aatayolu <ahmetatayolu@gmail.com>
2024-05-31 21:31:31 +0300

    testing-deneme-v2

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ from fastapi.security import HTTPAuthorizationCredentials
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-# Add CORS middlewarea
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
